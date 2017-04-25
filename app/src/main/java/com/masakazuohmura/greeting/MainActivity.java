f311fce47567bd7819a00cc77924f5115a2e0704
MasakazuOhmura <tourokusaito.masakazu@gmail.com>
2017-04-25 11:40:03 +0900

    easy greeting

--- app/src/main/java/com/masakazuohmura/greeting/MainActivity.java
+++ app/src/main/java/com/masakazuohmura/greeting/MainActivity.java
@@ -1,7 +1,11 @@
 package com.masakazuohmura.greeting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button greetingButton = (Button) findViewById(R.id.greeting_button);
+        final EditText greetingEdit = (EditText) findViewById(R.id.greeting_edit_text);
+        TextView greetingText = (TextView) findViewById(R.id.greeting_text);
+
+        greetingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String greetingStr = greetingEdit.getText().toString();
+                greetingEdit.setText(greetingStr);
+            }
+        });
     }
 }
